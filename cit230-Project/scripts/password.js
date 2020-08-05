afb4f562cb82198f793c2eddbe443439d88bf8d5
deturk <deg17003@byui.edu>
2020-08-05 11:49:04 -0600

    final Pro

---
+++ cit230-Project/scripts/password.js
@@ -0,0 +1,11 @@
+var password;
+var pass1="94051992";
+
+password=prompt('What\'s De-Graft code: Virus Hacker?',' ');
+
+if (password==pass1)
+  alert('Password Correct! Click OK to enter!');
+else
+   {
+    window.location="https://deturk.github.io/cit230/";
+    }
